7056644070d03850c30507e4f9ff89fadeee319a
sbot50 <mikebruens@tutanota.com>
2023-04-23 14:33:56 +0200

    fixed it with yes/no polls too now

--- Buttons/poll/yn.js
+++ Buttons/poll/yn.js
@@ -64,7 +64,7 @@ module.exports = {
       );
     let options = [];
     await message.guild.channels.cache.forEach((channel) => {
-      if (channel.type == 0) {
+      if (channel.type == 0 || channel.type == 5) {
         let option = {};
         option.label = channel.name;
         if (channel.parentId == null) {
