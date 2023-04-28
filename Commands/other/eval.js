2f798abdb0c519372a44147ae343adbfe3c22830
sbot50 <mikebruens@tutanota.com>
2023-04-28 17:50:00 +0200

    better autocomplete, fixed bug

--- Commands/other/eval.js
+++ Commands/other/eval.js
@@ -1,23 +1,30 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const Discord = require("discord.js");
 const tio = require("tryitonline");
+const levenshtein = require("fast-levenshtein").get;
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("eval")
 		.setDescription("Eval command for Bot Devs!")
 		.addStringOption((option) =>
-			option.setName("code").setDescription("Code to eval. use \\n for newline.").setRequired(true)
+			option
+				.setName("code")
+				.setDescription("Code to eval. use \\n for newline.")
+				.setRequired(true)
 		)
 		.addStringOption((option) =>
-			option.setName("language").setDescription("Coding language to use. (default is javascript-node)").setAutocomplete(true)
+			option
+				.setName("language")
+				.setDescription("Coding language to use. (default is javascript-node)")
+				.setAutocomplete(true)
 		),
 	permissions: [],
 	async execute({ client, args, guild, channel, member, interaction }) {
 		let code = args.code.replace("\\n", "\n");
-    if (!args.language) {
-      args.language = "javascript-node"
-    }
+		if (!args.language) {
+			args.language = "javascript-node";
+		}
 		let res = await tio.evaluate(
 			{
 				language: args.language,
@@ -32,8 +39,8 @@ module.exports = {
 				.setFooter({ text: "Used NPM: 'tryitonline'" });
 			await interaction.editReply({ content: " ", embeds: [embed] });
 		}
-    if (args.code.length > 1024) args.code = args.code.substr(1011) + "..."
-    if (res.output > 1024) res.output = res.output.substr(1011) + "..."
+		if (args.code.length > 1024) args.code = args.code.substr(1011) + "...";
+		if (res.output.length > 1024) res.output = res.output.substr(1011) + "...";
 		let embed = new EmbedBuilder()
 			.setColor(0x1cd0ce)
 			.addFields([
@@ -51,13 +58,17 @@ module.exports = {
 			.setFooter({ text: "Used NPM: 'tryitonline'" });
 		await interaction.editReply({ content: " ", embeds: [embed] });
 	},
-  async autocomplete({ interaction }) {
+	async autocomplete({ interaction }) {
 		let focusedValue = interaction.options.getFocused();
-    let choices = await tio.languages();
-    choices = choices.map(lang => lang.id);
-		let filtered = choices.filter(choice => choice.startsWith(focusedValue));
+		let choices = await tio.languages();
+		choices = choices.map((lang) => lang.id);
+		let filtered = choices
+			.map((lang) => ({ name: lang, dist: levenshtein(lang, focusedValue) }))
+			.sort((a, b) => a.dist - b.dist)
+			.slice(0, 25)
+			.map((lang) => lang.name);
 		await interaction.respond(
-			filtered.map(choice => ({ name: choice, value: choice })),
+			filtered.map((choice) => ({ name: choice, value: choice }))
 		);
 	},
 };
