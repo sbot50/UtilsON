81234e998ec0989fdd748f8d48ad1aedca468822
sbot50 <mikebruens@tutanota.com>
2023-04-28 23:04:16 +0200

    better autocomplete, actual language names

--- Commands/other/eval.js
+++ Commands/other/eval.js
@@ -11,7 +11,10 @@ const levenshtein = require("fast-levenshtein").get;
 let languages;
 (async () => {
 	let langs = await tio.languages();
-	languages = langs.map((lang) => lang.id);
+	languages = langs.reduce((acc, item) => {
+		acc[item.name] = item.id;
+		return acc;
+	  }, {});
 })();
 
 module.exports = {
@@ -27,8 +30,10 @@ module.exports = {
 	permissions: [],
 	dontDefer: true,
 	async execute({ client, args, guild, channel, member, interaction }) {
-		if (!args.language || !languages.includes(args.language)) {
+		if (!args.language || !Object.keys(languages).includes(args.language)) {
 			args.language = "javascript-node";
+		} else {
+			args.language = languages[args.language]
 		}
 		let modal = new ModalBuilder()
 			.setCustomId("eval")
@@ -43,64 +48,10 @@ module.exports = {
 
 		modal.addComponents(code);
 		interaction.showModal(modal);
-		return;
-		let languagename = args.language.split("-")[0];
-		let res = await tio.evaluate(
-			{
-				language: args.language,
-				code: args.code,
-			},
-			30000
-		);
-		if (res.status != "passed") {
-			let embed = new EmbedBuilder()
-				.setColor(0xa31600)
-				.addFields([{ name: "**ERROR**", value: "Timed Out!" }])
-				.setFooter({ text: "Used NPM: 'tryitonline'" });
-			await interaction.editReply({ content: " ", embeds: [embed] });
-		}
-		if (args.code.length > 1014 - languagename.length)
-			args.code = args.code.substr(0, 1011 - languagename.length) + "...";
-		if (res.output.length > 1014) res.output = res.output.substr(0, 1011) + "...";
-		if (res.warnings.length > 1014)
-			res.warnings = res.warnings.substr(0, 1011) + "...";
-		if (res.debug.length > 1014) res.debug = res.debug.substr(0, 1011) + "...";
-		let embed = new EmbedBuilder().setColor(0x1cd0ce).addFields([
-			{
-				name: "**Input**",
-				value: "```" + languagename + "\n" + args.code.replace(/`/g, "´") + "\n```",
-			},
-		]);
-		if (res.output.length > 0) {
-			embed.addFields([
-				{
-					name: "**Eval**",
-					value: "```\n" + res.output.replace(/`/g, "´") + "\n```",
-				},
-			]);
-		}
-		if (res.warnings.length > 0) {
-			embed.addFields([
-				{
-					name: "**Warnings**",
-					value: "```\n" + res.warnings.replace(/`/g, "´") + "\n```",
-				},
-			]);
-		}
-		if (res.debug.length > 0) {
-			embed.addFields([
-				{
-					name: "**Debug**",
-					value: "```\n" + res.debug.replace(/`/g, "´") + "\n```",
-				},
-			]);
-		}
-		embed.setFooter({ text: "Used NPM: 'tryitonline'" });
-		await interaction.editReply({ content: " ", embeds: [embed] });
 	},
 	async autocomplete({ interaction }) {
 		let focusedValue = interaction.options.getFocused();
-		let filtered = languages.filter((lang) => lang.startsWith(focusedValue));
+		let filtered = Object.keys(languages).filter((lang) => lang.startsWith(focusedValue) || lang.includes(focusedValue));
 		filtered = filtered
 			.map((lang) => ({ name: lang, dist: levenshtein(lang, focusedValue) }))
 			.sort((a, b) => a.dist - b.dist)
