000fc3c1409f0d0658508b7e04e12f3bb575a97a
sbot50 <xyz@xyz.xyz>
2023-04-28 18:46:39 +0200

    cached

--- Commands/other/eval.js
+++ Commands/other/eval.js
@@ -2,6 +2,11 @@ const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const Discord = require("discord.js");
 const tio = require("tryitonline");
 const levenshtein = require("fast-levenshtein").get;
+const languages = (async () => {
+	let langs = await tio.languages();
+	langs = langs.map((lang) => lang.id);
+	return langs;
+})();
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -22,7 +27,7 @@ module.exports = {
 	permissions: [],
 	async execute({ client, args, guild, channel, member, interaction }) {
 		args.code = args.code.replace(/\\n/g, "\n");
-		if (!args.language) {
+		if (!args.language || !languages.includes(args.language)) {
 			args.language = "javascript-node";
 		}
 		let res = await tio.evaluate(
@@ -39,30 +44,46 @@ module.exports = {
 				.setFooter({ text: "Used NPM: 'tryitonline'" });
 			await interaction.editReply({ content: " ", embeds: [embed] });
 		}
-		if (args.code.length > 1014) args.code = args.code.substr(1010) + "...";
-		if (res.output.length > 1014) res.output = res.output.substr(1010) + "...";
-		let embed = new EmbedBuilder()
-			.setColor(0x1cd0ce)
-			.addFields([
-				{
-					name: "**Input**",
-					value: "```\n" + args.code + "\n```",
-				},
-			])
-			.addFields([
+		if (args.code.length > 1014) args.code = args.code.substr(1011) + "...";
+		if (res.output.length > 1014) res.output = res.output.substr(1011) + "...";
+		if (res.warnings.length > 1014) res.warnings = res.warnings.substr(1011) + "...";
+		if (res.debug.length > 1014) res.debug = res.debug.substr(1011) + "...";
+		let embed = new EmbedBuilder().setColor(0x1cd0ce).addFields([
+			{
+				name: "**Input**",
+				value: "```\n" + args.code + "\n```",
+			},
+		]);
+		if (res.output.length > 0) {
+			embed.addFields([
 				{
 					name: "**Eval**",
 					value: "```\n" + res.output + "\n```",
 				},
-			])
-			.setFooter({ text: "Used NPM: 'tryitonline'" });
+			]);
+		}
+		if (res.warnings.length > 0) {
+			embed.addFields([
+				{
+					name: "**Warnings**",
+					value: "```\n" + res.warnings + "\n```",
+				},
+			]);
+		}
+		if (res.debug.length > 0) {
+			embed.addFields([
+				{
+					name: "**Debug**",
+					value: "```\n" + res.debug + "\n```",
+				},
+			]);
+		}
+		embed.setFooter({ text: "Used NPM: 'tryitonline'" });
 		await interaction.editReply({ content: " ", embeds: [embed] });
 	},
 	async autocomplete({ interaction }) {
 		let focusedValue = interaction.options.getFocused();
-		let choices = await tio.languages();
-		choices = choices.map((lang) => lang.id);
-    let filtered = choices.filter(choice => choice.startsWith(focusedValue));
+		let filtered = languages.filter((lang) => lang.startsWith(focusedValue));
 		filtered = filtered
 			.map((lang) => ({ name: lang, dist: levenshtein(lang, focusedValue) }))
 			.sort((a, b) => a.dist - b.dist)
