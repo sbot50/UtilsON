3e9af5657e186867f4b9dc696e0e6dd6a33b3a6d
sbot50 <xyz@zyx>
2023-12-13 12:18:34 +0100

    Files are hard

--- Commands/image/captcha.js
+++ Commands/image/captcha.js
@@ -57,11 +57,26 @@ module.exports = {
       //   registerFont("./Fonts/comic.ttf", { family: "Comic Sans MS" });
       //   registerFont("./Fonts/wingdings.ttf", { family: "Wingdings" });
       let fonts = [
-        fs.readFileSync("../../Fonts/arial.ttf").then(r => r.arrayBuffer()).then(b => new Uint8Array(b)),
-        fs.readFileSync("../../Fonts/calibri.ttf").then(r => r.arrayBuffer()).then(b => new Uint8Array(b)),
-        fs.readFileSync("../../Fonts/times_new_roman.ttf").then(r => r.arrayBuffer()).then(b => new Uint8Array(b)),
-        fs.readFileSync("../../Fonts/roadrage.ttf").then(r => r.arrayBuffer()).then(b => new Uint8Array(b)),
-        fs.readFileSync("../../Fonts/comic.ttf").then(r => r.arrayBuffer()).then(b => new Uint8Array(b)),
+        fs
+          .readFileSync("./Fonts/arial.ttf")
+          .then((r) => r.arrayBuffer())
+          .then((b) => new Uint8Array(b)),
+        fs
+          .readFileSync("./Fonts/calibri.ttf")
+          .then((r) => r.arrayBuffer())
+          .then((b) => new Uint8Array(b)),
+        fs
+          .readFileSync("./Fonts/times_new_roman.ttf")
+          .then((r) => r.arrayBuffer())
+          .then((b) => new Uint8Array(b)),
+        fs
+          .readFileSync("./Fonts/roadrage.ttf")
+          .then((r) => r.arrayBuffer())
+          .then((b) => new Uint8Array(b)),
+        fs
+          .readFileSync("./Fonts/comic.ttf")
+          .then((r) => r.arrayBuffer())
+          .then((b) => new Uint8Array(b)),
       ];
       //let ctx = canvas.getContext("2d");
       let p1 = image.width;
@@ -82,48 +97,48 @@ module.exports = {
         // ctx.fillText(char, loc1, loc2);
       }
       text = text.split("");
-    //   ctx.lineWidth = 3;
-    //   ctx.fillStyle = "rgb(0, 224, 15)";
-    //   ctx.strokeStyle = "rgb(235, 7, 30)";
-    //   let l1 = -1;
-    //   let l2 = -1;
-    //   let loc1 = 0;
-    //   let offset = (canvas.width - 100) / text.length;
-    //   let array = [];
-    //   for (let j = 0; j < text.length; j++) {
-    //     loc1 = loc1 + offset;
-    //     let loc2 = Math.random() * (p2 - 50) + 50;
-    //     l2 = l1;
-    //     l1 = [loc1, loc2];
-    //     let list = [text[j], loc1, loc2];
-    //     array.push(list);
-    //     if (l2 != -1) {
-    //       ctx.moveTo(l2[0], l2[1]);
-    //       ctx.lineTo(l1[0], l1[1]);
-    //       ctx.stroke();
-    //     }
-    //   }
-    //   for (let k = 0; k < array.length; k++) {
-    //     ctx.fillStyle =
-    //       "rgb(" +
-    //       rngNum() +
-    //       "," +
-    //       (Math.floor(Math.random() * 200) + 55) +
-    //       " ," +
-    //       rngNum() +
-    //       ")";
-    //     let rng = JSON.stringify(Math.floor(Math.random() * 25) + 25);
-    //     let rngfont = fonts[Math.floor(Math.random() * fonts.length)];
-    //     if (wingdingsmode == 1) {
-    //       rngfont = "Wingdings";
-    //     }
-    //     ctx.font = rng + "px " + rngfont;
-    //     let value = array[k];
-    //     let letter = value[0];
-    //     let loc1 = value[1];
-    //     let loc2 = value[2];
-    //     ctx.fillText(letter, loc1, loc2);
-    //   }
+      //   ctx.lineWidth = 3;
+      //   ctx.fillStyle = "rgb(0, 224, 15)";
+      //   ctx.strokeStyle = "rgb(235, 7, 30)";
+      //   let l1 = -1;
+      //   let l2 = -1;
+      //   let loc1 = 0;
+      //   let offset = (canvas.width - 100) / text.length;
+      //   let array = [];
+      //   for (let j = 0; j < text.length; j++) {
+      //     loc1 = loc1 + offset;
+      //     let loc2 = Math.random() * (p2 - 50) + 50;
+      //     l2 = l1;
+      //     l1 = [loc1, loc2];
+      //     let list = [text[j], loc1, loc2];
+      //     array.push(list);
+      //     if (l2 != -1) {
+      //       ctx.moveTo(l2[0], l2[1]);
+      //       ctx.lineTo(l1[0], l1[1]);
+      //       ctx.stroke();
+      //     }
+      //   }
+      //   for (let k = 0; k < array.length; k++) {
+      //     ctx.fillStyle =
+      //       "rgb(" +
+      //       rngNum() +
+      //       "," +
+      //       (Math.floor(Math.random() * 200) + 55) +
+      //       " ," +
+      //       rngNum() +
+      //       ")";
+      //     let rng = JSON.stringify(Math.floor(Math.random() * 25) + 25);
+      //     let rngfont = fonts[Math.floor(Math.random() * fonts.length)];
+      //     if (wingdingsmode == 1) {
+      //       rngfont = "Wingdings";
+      //     }
+      //     ctx.font = rng + "px " + rngfont;
+      //     let value = array[k];
+      //     let letter = value[0];
+      //     let loc1 = value[1];
+      //     let loc2 = value[2];
+      //     ctx.fillText(letter, loc1, loc2);
+      //   }
       let captcha = new AttachmentBuilder(Buffer.from(image), {
         name: "CAPTCHA.png",
       });
